e87feb9c9cd6f7f523c5445dcb2a17f025ee1076
Nikhil Sama <nikhilsama@gmail.com>
2024-04-17 14:42:50 +0530

    Add react to popup

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import './popup.css';
 
-const test = <p>Test</p>;
+const App: React.FC = () => {
+    useEffect(() => {
+        // Your code here
+    }, []);
+
+    return (
+        <div>
+            <h1>Popup</h1>
+            <p>Popup</p>
+        </div>
+    );
+}
 
 const root = document.createElement('div');
 document.body.appendChild(root);    
 const rootContainer = createRoot(root);
-rootContainer.render(test);
+rootContainer.render(<App />);
